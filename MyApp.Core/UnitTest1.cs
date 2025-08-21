412caa69f0f06555e368fb532f15f049672b6ebb
elsaliu2024career-star <elsaliu2024career@gmail.com>
2025-08-21 11:06:12 +1000

    CSFLCM-2:change file

---
+++ MyApp.Core/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿namespace MyApp.Core
+{
+    public class MathService
+    {
+        public int Multiply(int a, int b) => a * b;
+        public int Divide(int a, int b) 
+        {
+            if (b == 0) throw new DivideByZeroException();
+            return a / b;
+        }
+    }
+}
